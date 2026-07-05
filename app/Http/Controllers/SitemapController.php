a6e252c685e3e385e16eca7c3de2be6f6391c4a5
bleedingroses <almighty.ridho@gmail.com>
2026-07-05 04:38:45 +0000

    instal sitemap

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use App\Models\Service;
-use App\Models\Portfolio;
-use App\Models\Category;
-use Illuminate\Http\Response;
+use Illuminate\Support\Facades\URL;
+use Spatie\Sitemap\Tags\Url as SitemapUrl;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\SitemapIndex;
 
 class SitemapController extends Controller
 {
@@ -15,9 +16,12 @@ class SitemapController extends Controller
      */
     public function index()
     {
-        $content = $this->generateSitemapIndex();
-        
-        return response($content, 200)
+        $sitemapIndex = SitemapIndex::create()
+            ->add(URL::to('/sitemap-pages.xml'))
+            ->add(URL::to('/sitemap-blog.xml'))
+            ->add(URL::to('/sitemap-services.xml'));
+
+        return response($sitemapIndex->render(), 200)
             ->header('Content-Type', 'application/xml');
     }
 
@@ -26,54 +30,37 @@ class SitemapController extends Controller
      */
     public function pages()
     {
-        $pages = [
-            [
-                'url' => url('/'),
-                'lastmod' => now()->toDateString(),
-                'changefreq' => 'daily',
-                'priority' => '1.0'
-            ],
-            [
-                'url' => url('/about'),
-                'lastmod' => now()->toDateString(),
-                'changefreq' => 'monthly',
-                'priority' => '0.8'
-            ],
-            [
-                'url' => url('/services'),
-                'lastmod' => now()->toDateString(),
-                'changefreq' => 'weekly',
-                'priority' => '0.9'
-            ],
-            [
-                'url' => url('/portfolio'),
-                'lastmod' => now()->toDateString(),
-                'changefreq' => 'weekly',
-                'priority' => '0.8'
-            ],
-            [
-                'url' => url('/blog'),
-                'lastmod' => now()->toDateString(),
-                'changefreq' => 'daily',
-                'priority' => '0.9'
-            ],
-            [
-                'url' => url('/timeline'),
-                'lastmod' => now()->toDateString(),
-                'changefreq' => 'monthly',
-                'priority' => '0.6'
-            ],
-            [
-                'url' => url('/contact'),
-                'lastmod' => now()->toDateString(),
-                'changefreq' => 'monthly',
-                'priority' => '0.7'
-            ],
-        ];
+        $sitemap = Sitemap::create()
+            ->add(SitemapUrl::create(URL::to('/'))
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(SitemapUrl::CHANGE_FREQUENCY_DAILY)
+                ->setPriority(1.0))
+            ->add(SitemapUrl::create(URL::to('/about'))
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(SitemapUrl::CHANGE_FREQUENCY_MONTHLY)
+                ->setPriority(0.8))
+            ->add(SitemapUrl::create(URL::to('/services'))
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(SitemapUrl::CHANGE_FREQUENCY_WEEKLY)
+                ->setPriority(0.9))
+            ->add(SitemapUrl::create(URL::to('/portfolio'))
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(SitemapUrl::CHANGE_FREQUENCY_WEEKLY)
+                ->setPriority(0.8))
+            ->add(SitemapUrl::create(URL::to('/blog'))
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(SitemapUrl::CHANGE_FREQUENCY_DAILY)
+                ->setPriority(0.9))
+            ->add(SitemapUrl::create(URL::to('/timeline'))
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(SitemapUrl::CHANGE_FREQUENCY_MONTHLY)
+                ->setPriority(0.6))
+            ->add(SitemapUrl::create(URL::to('/contact'))
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(SitemapUrl::CHANGE_FREQUENCY_MONTHLY)
+                ->setPriority(0.7));
 
-        $content = $this->generateUrlSet($pages);
-        
-        return response($content, 200)
+        return response($sitemap->render(), 200)
             ->header('Content-Type', 'application/xml');
     }
 
@@ -82,21 +69,21 @@ class SitemapController extends Controller
      */
     public function blog()
     {
-        $articles = Article::published()
+        $sitemap = Sitemap::create();
+
+        Article::published()
             ->orderBy('updated_at', 'desc')
             ->get()
-            ->map(function ($article) {
-                return [
-                    'url' => url('/blog/' . $article->slug),
-                    'lastmod' => $article->updated_at->toDateString(),
-                    'changefreq' => 'weekly',
-                    'priority' => $article->is_featured ? '0.9' : '0.7'
-                ];
+            ->each(function ($article) use ($sitemap) {
+                $sitemap->add(
+                    SitemapUrl::create(URL::to('/blog/' . $article->slug))
+                        ->setLastModificationDate($article->updated_at)
+                        ->setChangeFrequency(SitemapUrl::CHANGE_FREQUENCY_WEEKLY)
+                        ->setPriority($article->is_featured ? 0.9 : 0.7)
+                );
             });
 
-        $content = $this->generateUrlSet($articles->toArray());
-        
-        return response($content, 200)
+        return response($sitemap->render(), 200)
             ->header('Content-Type', 'application/xml');
     }
 
@@ -105,67 +92,18 @@ class SitemapController extends Controller
      */
     public function services()
     {
-        $services = Service::all()
-            ->map(function ($service) {
-                return [
-                    'url' => url('/services/' . $service->slug),
-                    'lastmod' => $service->updated_at->toDateString(),
-                    'changefreq' => 'monthly',
-                    'priority' => '0.8'
-                ];
-            });
+        $sitemap = Sitemap::create();
 
-        $content = $this->generateUrlSet($services->toArray());
-        
-        return response($content, 200)
+        Service::query()->get()->each(function ($service) use ($sitemap) {
+            $sitemap->add(
+                SitemapUrl::create(URL::to('/services/' . $service->slug))
+                    ->setLastModificationDate($service->updated_at)
+                    ->setChangeFrequency(SitemapUrl::CHANGE_FREQUENCY_MONTHLY)
+                    ->setPriority(0.8)
+            );
+        });
+
+        return response($sitemap->render(), 200)
             ->header('Content-Type', 'application/xml');
-    }
-
-    /**
-     * Generate sitemap index XML
-     */
-    private function generateSitemapIndex(): string
-    {
-        $sitemaps = [
-            url('/sitemap-pages.xml'),
-            url('/sitemap-blog.xml'),
-            url('/sitemap-services.xml'),
-        ];
-
-        $xml = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
-        $xml .= '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
-
-        foreach ($sitemaps as $sitemap) {
-            $xml .= '  <sitemap>' . PHP_EOL;
-            $xml .= '    <loc>' . htmlspecialchars($sitemap) . '</loc>' . PHP_EOL;
-            $xml .= '    <lastmod>' . now()->toDateString() . '</lastmod>' . PHP_EOL;
-            $xml .= '  </sitemap>' . PHP_EOL;
-        }
-
-        $xml .= '</sitemapindex>';
-
-        return $xml;
-    }
-
-    /**
-     * Generate URL set XML
-     */
-    private function generateUrlSet(array $urls): string
-    {
-        $xml = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
-        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
-
-        foreach ($urls as $url) {
-            $xml .= '  <url>' . PHP_EOL;
-            $xml .= '    <loc>' . htmlspecialchars($url['url']) . '</loc>' . PHP_EOL;
-            $xml .= '    <lastmod>' . $url['lastmod'] . '</lastmod>' . PHP_EOL;
-            $xml .= '    <changefreq>' . $url['changefreq'] . '</changefreq>' . PHP_EOL;
-            $xml .= '    <priority>' . $url['priority'] . '</priority>' . PHP_EOL;
-            $xml .= '  </url>' . PHP_EOL;
-        }
-
-        $xml .= '</urlset>';
-
-        return $xml;
     }
 }
